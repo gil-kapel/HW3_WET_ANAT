af708d6d5be2ec5ba547ed171bc7609d7dbd30d2
Gil Kapel <gil.kapel@gmail.com>
2022-06-08 13:56:10 +0300

    part 2 and part 3 - not good results

--- code/hw3_part2.py
+++ code/hw3_part2.py
@@ -2,4 +2,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import glob
+import os
+from utils import show_single_graph, calc_mse
 
+
+def question2():
+    # ---------------------------- section 2.a -------------------------------
+    # --------------------------------------------- section 2.1 --------------
+    image_list = []
+    os.chdir("../given_data/LFW")
+    for filename in glob.glob("*.pgm"):
+        image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        image_list.append(image)
+    image_list = np.array(image_list)
+    for i, image in enumerate(image_list):
+        if i > 3:
+            break
+        cv2.imshow(f'Image {i}', image)
+
+    # --------------------------------------------- section 2.2 -------------
+    column_stack_image = image_list.reshape((image_list.shape[1] * image_list.shape[2], image_list.shape[0]), order='F')
+
+    # --------------------------------------------- section 2.3 -------------
+    avg_image = np.mean(column_stack_image, axis=1)
+    image_shape = (image_list.shape[1], image_list.shape[2])
+    mat_avg_image = avg_image.reshape(image_shape, order='F')
+    cv2.imshow('Average image', mat_avg_image)
+    mu = avg_image.reshape((avg_image.shape[0], 1))
+    y = column_stack_image - mu
+    y_cov = np.cov(y)
+    cv2.destroyAllWindows()
+
+    # ---------------------------- section 2.b -------------------------------
+    # --------------------------------------------- section 2.1 --------------
+    eig_vals, eig_vecs = np.linalg.eigh(y_cov)
+    eig_vals, eig_vecs = eig_vals[::-1][:10], eig_vecs[::-1][:10]
+    x = np.linspace(0, 10, 10)
+    plt.plot(x, eig_vals)
+    plt.title('Eigen values')
+    plt.show()
+    plt.close()
+    for i, vector in enumerate(eig_vecs):
+        if i > 3:
+            break
+        cv2.imshow('Eigen image', vector.reshape(image_shape, order='F'))
+
+    # ---------------------------- section 2.c -------------------------------
+    P = eig_vecs @ y[:, :4]
+
+    # ---------------------------- section 2.d -------------------------------
+    x_hat = eig_vecs.T @ P + mu
+    x_b = image_list[:4].reshape(x_hat.shape, order='F')
+    mse = calc_mse(x_b, x_hat)
+    for i, vector in enumerate(x_b.T):
+        if i > 3:
+            break
+        cv2.imshow(f'restored image, MSE= {mse[i]}', vector.reshape(image_shape, order='F'))
+    pass
+
+    # ---------------------------- section 2.e -------------------------------
+    eig_vals_e, eig_vecs_e = eig_vals[::-1][:10], eig_vecs[::-1][:10]
+    P_e = eig_vecs_e @ y[:, :4]
+    x_hat_e = eig_vecs.T @ P + mu
+    x_b_e= image_list[:4].reshape(x_hat_e.shape, order='F')
+    mse_e = ((x_hat_e - x_b_e) ** 2).mean(axis=0).mean(axis=0)
+    for i, vector in enumerate(x_b_e.T):
+        if i > 3:
+            break
+        cv2.imshow(f'restored image, MSE= {mse_e[i]}', vector.reshape(image_shape, order='F'))
+
+
+if __name__ == '__main__':
+    question2()
+    cv2.destroyAllWindows()
--- code/hw3_part3.py
+++ code/hw3_part3.py
@@ -1,5 +1,126 @@
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import glob
+from utils import calc_mse
+
+
+def pyr_gen(n, m, img, gauss_pyr, laplace_pyr):
+    """
+    Constructs Gaussian and Laplacian pyramids out of an image.
+    :param n: number of pyramid levels
+    (excluding the 0th level - total number of levels - n+1).
+    :param m: current pyramid level
+    :param img: The input gray-scale image.
+    The m-1 level of the Gaussian pyramid.
+    np array of of type uint8.
+    :param gauss_pyr: The Gaussian pyramid so far.
+    Python list of length [m-1] containing the pyramid levels
+    as np arrays of type int.
+    :param laplace_pyr: The Laplacian pyramid so far.
+    Python list of length [m-1] containing the pyramid levels
+    as np arrays of type int.
+    :return:
+    gauss_pyr: The Gaussian pyramid.
+    Python list of length [n-m+1] containing the pyramid levels
+    as np arrays of type int.
+    laplace_pyr: The Laplacian pyramid.
+    Python list of length [n-m+1] containing the pyramid levels
+    as np arrays of type int.
+    """
+    assert m >= 0 and m <= n
+    # ====== YOUR CODE: ======
+    # ========================
+    if n == m:
+        gauss_pyr.append(img)
+        laplace_pyr.append(img)
+        return gauss_pyr, laplace_pyr
+
+    gauss_pyr.append(img)
+    next_gauss_pyr = cv2.pyrDown(img)
+
+    laplace_pyr.append(img.astype('int') - cv2.pyrUp(next_gauss_pyr).astype('int'))
+    gauss_pyr, laplace_pyr = pyr_gen(n, m+1, next_gauss_pyr, gauss_pyr, laplace_pyr)
+    return gauss_pyr, laplace_pyr
+
+
+def laplace_recon(laplace_pyr):
+    """
+    Image reconstruction from Laplacian pyramid.
+    :param laplace_pyr: The Laplacian pyramid.
+    Python list containing the pyramid levels
+    as np arrays of type int.
+    :return:
+    recon_img: The reconstructed image.
+    2D np array of the same shape as laplace_pyr[0].
+    """
+    # ====== YOUR CODE: ======
+    # ========================
+    recon_img = np.zeros(laplace_pyr[0].shape)
+    for i, image in enumerate(laplace_pyr):
+        image_range = np.max(image)-np.min(image)
+        if i != len(laplace_pyr) - 1:
+            image = (np.array((image + np.abs(np.min(image))) * 255 / image_range)).astype('uint8')
+        for j in range(i):
+            image = cv2.pyrUp(image)
+        recon_img += image
+    return recon_img.astype('uint8')
+
+
+def question3():
+    # ---------------------------- section 3.a -------------------------------
+
+    ironman = cv2.cvtColor(cv2.imread("../given_data/Ironman.jpg"), cv2.COLOR_BGR2GRAY)
+    downey = cv2.cvtColor(cv2.imread("../given_data/Downey.jpg"), cv2.COLOR_BGR2GRAY)
+    n = 4
+    ironman_gauss_pyr, ironman_laplace_pyr = pyr_gen(n, 0, ironman, [], [])
+    for image in ironman_gauss_pyr:
+        image = np.array(image)
+        cv2.imwrite(f"../my_data/q3/IronMan_Gauss_image{image.shape[0]}.jpeg", image)
+    for i, image in enumerate(ironman_laplace_pyr):
+        image_range = np.max(image)-np.min(image)
+        img = image
+        if i != len(ironman_laplace_pyr) - 1:
+            img = (np.array((image + np.abs(np.min(image))) * 255 / image_range)).astype('uint8')
+        cv2.imwrite(f"../my_data/q3/IronMan_Laplace_image{img.shape[0]}.jpeg", img)
+
+    downey_gauss_pyr, downey_laplace_pyr = pyr_gen(n, 0, downey, [], [])
+    for image in downey_gauss_pyr:
+        image = np.array(image)
+        cv2.imwrite(f"../my_data/q3/Downey_Gauss_image{image.shape[0]}.jpeg", image)
+    for i, image in enumerate(downey_laplace_pyr):
+        image_range = np.max(image)-np.min(image)
+        img = image
+        if i != len(downey_laplace_pyr) - 1:
+            img = (np.array((image + np.abs(np.min(image))) * 255 / image_range)).astype('uint8')
+        cv2.imwrite(f"../my_data/q3/Downey_Laplace_image{img.shape[0]}.jpeg", img)
+
+    # ---------------------------- section 3.b -------------------------------
+    ironman_recon = laplace_recon(copy.deepcopy(ironman_laplace_pyr))
+    downey_recon = laplace_recon(copy.deepcopy(downey_laplace_pyr))
+    ironman_mse = calc_mse(ironman_recon, ironman)
+    downey_mse = calc_mse(downey_recon, downey)
+    cv2.imshow(f'ironman_recon with mse:{ironman_mse}', ironman_recon)
+    cv2.imshow(f'downey_recon with mse:{downey_mse}', downey_recon)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # ---------------------------- section 3.c -------------------------------
+    mask = np.zeros(downey.shape)
+    mid_img = int(mask.shape[0]/2)
+    mask[:, :mid_img] = 1
+    mask_gauss_pyr, mask_laplace_pyr = pyr_gen(n, 0, mask, [], [])
+    pass
+    blend_laplace = np.array(ironman_laplace_pyr) * np.array(mask_laplace_pyr) + \
+                    (1 - np.array(mask_laplace_pyr)) * np.array(downey_laplace_pyr)
+    blend_recon = laplace_recon(copy.deepcopy(blend_laplace))
+    blend_mse = calc_mse(blend_recon, ironman)
+    cv2.imshow(f'blend_mse with mse:{blend_mse}', blend_recon)
+    pass
+
+
+if __name__ == '__main__':
+    question3()
+    cv2.destroyAllWindows()
 
